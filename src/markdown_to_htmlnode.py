75fa8d8f77070bce032a178e2e61cc9e59194132
Christopher Caquelin <caquelin@hey.com>
2025-09-04 15:16:58 +0000

    Working markdown_to_htmlnode

--- src/markdown_to_htmlnode.py
+++ src/markdown_to_htmlnode.py
@@ -1,4 +1,4 @@
-from src.htmlnode import ParentNode
+from src.htmlnode import ParentNode, LeafNode
 from src.text_to_children import text_to_children
 from src.block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
@@ -10,5 +10,21 @@ def markdown_to_htmlnode(markdown: str):
         if block_to_block_type(block) == BlockType.PARAGRAPH:
             text = " ".join(block.splitlines())
             children = text_to_children(text)
-            nodes.append(ParentNode("p", children))
+            para = ParentNode("p", children)
+            nodes.append(para)
+        if block_to_block_type(block) == BlockType.CODE:
+            lines = block.splitlines()
+            inner = "\n".join(lines[1:-1]) + "\n"
+            code_leaf = LeafNode("code", inner)
+            pre = ParentNode("pre", [code_leaf])
+            nodes.append(pre)
+        if block_to_block_type(block) == BlockType.HEADING:
+            first = block.splitlines()[0].lstrip()
+            level = 0
+            while level < len(first) and first[level] == "#" and level < 6:
+                level += 1
+            text = first[level + 1:]
+            children = text_to_children(text)
+            nodes.append(ParentNode(f"h{level}", children))
+
     return ParentNode("div", nodes)
--- src/test_markdown_to_htmlnode.py
+++ src/test_markdown_to_htmlnode.py
@@ -3,7 +3,7 @@ from src.markdown_to_htmlnode import markdown_to_htmlnode
 
 
 class TestMarkdownToHTMLNode(unittest.TestCase):
-    def test_paragraphs(self):
+    def test_paragraph(self):
         md = """
 This is **bolded** paragraph
 text in a p
@@ -19,3 +19,19 @@ This is another paragraph with _italic_ text and `code` here
             html,
         "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
         )
+
+    def test_code(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+
+"""
+
+        node = markdown_to_htmlnode(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+        "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+    )
